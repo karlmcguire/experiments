8726d9f36368679a56c92d87412d8a52804afad9
Karl McGuire <karl@xba.io>
2019-11-15 15:12:14 -0800

    proof of concept

--- expiration/expiration.go
+++ expiration/expiration.go
@@ -9,14 +9,14 @@ import (
 type Cache struct {
 	sync.RWMutex
 	times    *list.List
-	data     map[uint64]uint64
+	data     map[uint64][2]uint64
 	used     uint64
 	capacity uint64
 }
 
 func NewCache(capacity uint64) *Cache {
 	return &Cache{
-		data:     make(map[uint64]uint64),
+		data:     make(map[uint64][2]uint64),
 		times:    list.New(),
 		capacity: capacity,
 	}
@@ -26,7 +26,7 @@ func (c *Cache) Get(key uint64) uint64 {
 	c.RLock()
 	val := c.data[key]
 	c.RUnlock()
-	return val
+	return val[0]
 }
 
 func (c *Cache) Set(key, val, cost, ttl uint64) []uint64 {
@@ -40,22 +40,32 @@ func (c *Cache) Set(key, val, cost, ttl uint64) []uint64 {
 	if room = c.capacity - c.used; room > cost {
 		goto add
 	}
-	for ; room < cost; room = c.capacity - c.used {
-		// check if items with set TTLs have expired first
-		for e := c.times.Back(); e != nil; e = e.Prev() {
-			item := e.Value.([3]uint64)
-			if item[2] < uint64(time.Now().Unix()) {
-				c.times.Remove(e)
-				delete(c.data, item[0])
-				c.used -= item[1]
-				victims = append(victims, item[0])
-			}
+	// check if items with set TTLs have expired first
+	for e := c.times.Back(); e != nil; e = e.Prev() {
+		item := e.Value.([3]uint64)
+		if item[2] > uint64(time.Now().Unix()) {
+			break
 		}
+		c.times.Remove(e)
+		delete(c.data, item[0])
+		c.used -= item[1]
+		victims = append(victims, item[0])
+	}
+	for ; room < cost; room = c.capacity - c.used {
 		// regular cost-based eviction
-		// TODO
+		/*
+			i := 0
+			minKey, minCost := uint64(0), uint64(0)
+			for k, v := range c.data {
+				// TODO
+				if i++; i == 5 {
+					break
+				}
+			}
+		*/
 	}
 add:
-	c.data[key] = val
+	c.data[key] = [2]uint64{val, cost}
 	c.used += cost
 	if ttl > 0 {
 		c.times.PushFront([3]uint64{
